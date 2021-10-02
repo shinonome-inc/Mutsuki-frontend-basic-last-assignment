8e63d2900d93f15b9613ffd6b4d7fc8365977292
Mutsuki Minezaki <m.minezaki@4nonome.com>
2021-10-02 21:14:38 +0900

    課題：変数を定義する
    初めてのコミットです

---
+++ src/stories/atoms-button.stories.js
@@ -0,0 +1,6 @@
+import atoms_button from "../components/atoms/button/button.html";
+import "../components/atoms/button/button.scss";
+
+export default { title: "atoms/button" };
+
+export const button = () => atoms_button;
