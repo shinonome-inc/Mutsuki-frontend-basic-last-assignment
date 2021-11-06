a84146dda77b50d4df2571916d773e8e93c34a6b
Mutsuki Minezaki <m.minezaki@4nonome.com>
2021-11-06 21:27:37 +0900

    Revert "button 3"
    This reverts commit 867539373710e7290e09533ce3476d9747123954.

--- src/stories/molecules-news-and-events-title.stories.js
+++
@@ -1,6 +0,0 @@
-import Molecules_NewsAndEventsTitle from "../components/molecules/news-and-events-title/news-and-events-title.html";
-import "../components/molecules/news-and-events-title/news-and-events-title.scss";
-
-export default { title: "Molecules/news-and-events-title" }
-
-export const NewsAndEventsTitle = () => Molecules_NewsAndEventsTitle;
