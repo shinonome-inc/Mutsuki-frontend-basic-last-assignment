bdbe980e59f1fbbc4e95847248771ac83f8141f2
Mutsuki Minezaki <m.minezaki@4nonome.com>
2021-11-15 11:46:51 +0900

    molecules-news-and-events-more.stories.jsファイル作成。

---
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -0,0 +1,6 @@
+import Molecules_NewsAndEventsMore from "../components/molecules/message/news-and-events-more.html";
+import "../components/molecules/message/news-and-events-more.scss";
+
+export default { title: "Molecules/NewsAndEventsMore" }
+
+export const NewsAndEventsMore = () => Molecules_NewsAndEventsMore;
