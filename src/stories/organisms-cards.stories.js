1beec485e2d39e0817ca004c51eb0c08913c93a6
Mutsuki Minezaki <m.minezaki@4nonome.com>
2022-02-11 15:50:38 +0900

    ファイル作成

---
+++ src/stories/organisms-cards.stories.js
@@ -0,0 +1,7 @@
+import Organisms_Cards from "../components/organisms/cards/cards.html";
+import "../components/organisms/cards/cards.scss";
+
+export default { title: "Organisms/Cards" }
+
+export const Cards = () => Organisms_Cards;
+
